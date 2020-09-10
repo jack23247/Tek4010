0be57e5a6e9825729071ba5198cd229a3136d3a9
Jacopo Maltagliati <j.maltagliati@campus.unimib.it>
2020-09-10 23:53:27 +0200

    Fix weird mistake

---
+++ makestuff.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo " \!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_@abcdefghijklmnopqrstuvwxyz["; cat 01_barchart.plt
---
+++ testfont.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+make && ./tek4010 -noexit ./makestuff.sh
